6962915ef2eca7ae5369934431205031aac7b3fd
will <willhargrave@hotmail.co.uk>
2023-03-01 15:40:37 +0900

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
-
+import Home from './components/Home';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import Quiz from './components/Quiz';
+import Quizes from './components/Quizes';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar>
         <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/quizes/" element={<Quizes/>} />
         <Route path="/quiz/:id" element={<Quiz/>} />
         </Routes>
+        </Navbar>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,39 @@
+import { Link } from "react-router-dom"
+import React from 'react';
 const Navbar = () => {
-
+    return (
+        <div>
+            <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <Link to="/">
+                <a className="navbar-brand">Quiz</a>
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link to="/login">
+                                <a className="nav-link">Login</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/quiz">
+                                <a className="nav-link">Your quiz</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/quizes">
+                                <a className="nav-link">All quizes</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="Github.com/willhargrave">Github</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
 }
 export default Navbar
