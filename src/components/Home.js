6cd6fd88414e16557ca313ce7b69207255a868e3
will <willhargrave@hotmail.co.uk>
2023-03-05 16:38:13 +0900

    Added functions

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,14 @@ import React from "react"
 import "../styles/Home.css"
 import { db } from "../config";
 import { uid } from 'uid';
-import { set, ref } from "firebase/database";
+import {useAuthState} from 'react-firebase-hooks/auth'
+import { set, ref as sRef, serverTimestamp } from "firebase/database";
 import { useState, useEffect } from "react";
-const Home = () => {
-const [todo, setTodo] = useState("");
 
-const handleTodoChange=(e)=>{
-    setTodo(e.target.value)
-}
-    const writeToDatabase = () => {
-        const uuid = uid()
-        set(ref(db, `/${uuid}`), {
-          todo,
-          uuid,
-        });
-        setTodo("");
-    };
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+function Home() {
+const [user] = useAuthState(auth)
 return(
     <div className="main">
        <div className="home-title">
@@ -29,6 +21,61 @@ return(
     </div>
 )
 }
+
+function questionDisplay() {
+    const dummy = useRef();
+    const questionsRef = firestore.collection('questions');
+    const query = questionsRef.orderBy('createdAt')
+
+    const [questions] = useCollectionData(query, {idField: 'id'})
+    const[typeValue, setTypeValue] = useState('')
+    const[questionValue, setQuestionValue] = useState('')
+    const timestamp = serverTimestamp();
+    const setQuestion = async(e) => {
+        e.preventDefault();
+        const {uid} = auth.currentUser;
+        await questionsRef.add({
+            type: typeValue,
+            question: questionValue,
+            createdAt: timestamp,
+            uid
+        })
+    }
+    return (
+        <div>
+            <main>
+                {questions && questions.map(qst => <MakeQuestion key={qst.id} question={qst} />)}
+                <div ref={dummy}></div>
+            </main>
+            <form onSubmit={setQuestion}>
+                <input value={typeValue} onChange={(e) => setTypeValue(e.target.value)}/>
+                <input value={questionValue} onChange={(e) => setQuestionValue(e.target.value)}/>
+            </form>
+        </div>
+    )
+}
+
+function MakeQuestion(props) {
+    const {text, uid} = props.question
+
+    const questionClass = uid === auth.currentUser.uid ? 'sent' : 'recieved';
+    return (
+        <div className={`question ${questionClass}`}>
+            <p>{type}: {question}</p>
+        </div>
+    )
+}
+function SignIn() {
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider);
+    }
+    return ( 
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    )
+  }
+
+
 export default Home
 
 
