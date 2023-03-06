53e173ec1875468250136d928f89d46e08c2e6ff
will <willhargrave@hotmail.co.uk>
2023-03-06 14:22:54 +0900

    question-display

--- src/components/Home.js
+++ src/components/Home.js
@@ -50,15 +50,22 @@ function QuestionDisplay() {
         <div>
             <main>
                 {questions && questions.map(qst => <MakeQuestion key={qst.id} question={qst} />)}
+                {questions && questions.map(qst => <AnswerQuestions key={qst.id} question={qst} />)}
                 <div ref={dummy}></div>
             </main>
             <form onSubmit={setQuestion}>
+                <div className="question-card">
+                    <div>
                 <input value={questionValue} placeholder="question"onChange={(e) => setQuestionValue(e.target.value)}/>
+                </div>
+                <div>
                 <input value={correctValue} placeholder="correct answer" onChange={(e) => setCorrectValue(e.target.value)}/>
                 <input value={firstChoiceValue} placeholder="wrong answer" onChange={(e) => setFirstChoiceValue(e.target.value)}/>
                 <input value={secondChoiceValue} placeholder="wrong answer" onChange={(e) => setSecondChoiceValue(e.target.value)}/>
                 <input value={thirdChoiceValue} placeholder="wrong answer" onChange={(e) => setThirdChoiceValue(e.target.value)}/>
+                </div>
                 <button type="submit">Go</button>
+                </div>
            </form>
         </div>
     )
@@ -81,6 +88,38 @@ function MakeQuestion(props) {
             <p>{thirdChoice}</p>
             </div>
             </div>
+        </div>
+    )
+}
+function AnswerQuestions(props) {
+    const {firstChoice, secondChoice, thirdChoice, correct, question, uid} = props.question
+    return (
+        <div>
+             <p>Q: {question}</p>
+
+             <span>
+                    <input type="radio" id="option-one" name="option" class="radio" value={firstChoice} />
+                    <label for="option-one" class="option" id="option-one-label">{firstChoice}</label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-two" name="option" class="radio" value="optionB" />
+                    <label for="option-two" class="option" id="option-two-label">{correct}</label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-three" name="option" class="radio" value="optionC" />
+                    <label for="option-three" class="option" id="option-three-label">{secondChoice}</label>
+                </span>
+
+
+                <span>
+                    <input type="radio" id="option-four" name="option" class="radio" value="optionD" />
+                    <label for="option-four" class="option" id="option-four-label">{thirdChoice}</label>
+                </span>
+              <button type="submit"></button>
         </div>
     )
 }
